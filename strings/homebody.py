979ee1f88cbcb15e26e673a2ad3cc75347ab7b65
Joshua Rodriguez <ewized@gmail.com>
2014-09-30 08:13:10 -0700

    Added homebody.py

---
+++ strings/homebody.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''
+file: homebody.py
+name: Joshua Rodriguez
+email: ewized@gmail.com
+problem: chapter 4 exercise 2 page 219
+description: Given the string 'homebody':
+    a) Print splice "home"
+    b) Print splice "body"
+'''
+
+homebody = "homebody"
+
+print(homebody[:4])
+print(homebody[4:])
+

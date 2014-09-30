9d87dbc9e0ec615753f8b49bf537c014308c7769
Joshua Rodriguez <ewized@gmail.com>
2014-09-30 09:40:05 -0700

    Added counting.py

---
+++ strings/counting.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+file: counting.py
+name: Joshua Rodriguez
+email: ewized@gmail.com
+problem: chapter 4 exercise 20 page 221
+description: Play around with the `count` method.
+    What does it count?
+
+    string = "Hello World"
+    string.count("o")
+'''
+
+string = "Year4000 Network | mc.year4000.net"
+
+# Counts the number of e's in the String
+print("Counting `e`:", string.count("e"))
+
+# Counts the number of 0's in the string
+print("Counting `0`:", string.count("0"))
+

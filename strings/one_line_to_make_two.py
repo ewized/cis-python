e8753dd7ed0cd37ab511789787cd599f988d4ca8
Joshua Rodriguez <ewized@gmail.com>
2014-09-30 08:46:04 -0700

    Added one_line_to_make_two.py

---
+++ strings/one_line_to_make_two.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+file: one_line_to_make_two.py
+name: Joshua Rodriguez
+email: ewized@gmail.com
+problem: chapter 4 exercise 13 page 220
+description: We know the following the code will print out
+    print("Line one")
+    print("Line two")
+
+    will make
+    
+    Line one
+    Line two
+
+    How to do this in one line.
+'''
+
+print("Line one \nLine two")
+

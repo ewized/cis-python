1dad217261b99d27072878f99e977b1cbebc6e88
Joshua Rodriguez <ewized@gmail.com>
2014-12-09 10:15:35 -0800

    Header for stocks

---
+++ stocks/stocks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''
+file: stocks.py
+name: Joshua Rodriguez
+email: ewized@gmail.com
+problem: number 1 page 379
+description: Make a program that mines Google's stocks, it will
+    calculate the monthly average from 2004 - 2008, then output
+    the best and worst six months.
+'''

188c59f886d63eeb507a1235ddad25c67a5d4604
Joshua Rodriguez <ewized@gmail.com>
2014-12-09 10:17:52 -0800

    Added header for class design

---
+++ class/class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''
+file: class.py
+name: Joshua Rodriguez
+email: ewized@gmail.com
+description: Design a class schedule for classes that I'm
+    a student during a semester.
+'''

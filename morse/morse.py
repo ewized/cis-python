ec386efd2a652d2f1bd04abc9f3ed45e456fde5f
Joshua Rodriguez <ewized@gmail.com>
2014-12-09 10:25:36 -0800

    Added header for morse code

---
+++ morse/morse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''
+file: morse.py
+name: Joshua Rodriguez
+email: ewized@gmail.com
+problem: number 21 page 432
+description: Make a program that converts strings to morse code
+    and convert morse code to strings. Create a dictionary to
+    map the chars to the morse code equivalent.
+'''

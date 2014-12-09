40078a8745d5729189209c684eb8ac512c71e88c
Joshua Rodriguez <ewized@gmail.com>
2014-12-09 10:25:46 -0800

    Added header for spreadsheets

---
+++ spreadsheets/spreadsheets.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+file: spreadsheets.py
+name: Joshua Rodriguez
+email: ewized@gmail.com
+description: Create a python program to edit CSV files.
+    Must do the following tasks
+    - Print the date
+    - Delete a row or column
+    - Insert a row or column
+    - Change a value in a cell
+    - Output the data in CSV format
+'''
